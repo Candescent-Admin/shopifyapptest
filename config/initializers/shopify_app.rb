80bad2c11fcde0459d7be1d9af970203604be0b3
Candescent-Admin <info@candescent.ca>
2016-11-22 21:40:54 -0500

    Update shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,7 @@ ShopifyApp.configure do |config|
 
   config.api_key = ENV['API_KEY']
   config.secret = ENV['API_SECRET']
+  config.redirect_url = 'http://shopifyapptest.herokuapp.com/auth/shopify/callback'
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
